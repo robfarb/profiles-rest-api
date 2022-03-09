fd80db8d7fe5250cc76d8dd72c49e43519be5a58
robfarb <rfarb@goodwatercap.com>
2022-03-08 16:33:56 -0800

    Add POST, PUT, PATCH, and DELETE views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloApiView(APIView):
   """Test Api View"""
+  seralizer_class = serializers.HelloSerializer
   
   def get(self, request, format=None):
     """Returns a list of APIView features"""
@@ -14,6 +18,41 @@ class HelloApiView(APIView):
     ]
 
     return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+  def post(self, request):
+    """Create a hello message with our name"""
+    serializer = self.seralizer_class(data=request.data)
+    
+    if serializer.is_valid():
+      name = serializer.validated_data.get('name')
+      message = f'Hello {name}'
+      return Response({'message': message})
+    else:
+      return Response(
+        serializer.errors,
+        status=status.HTTP_400_BAD_REQUEST,
+      )
+
+  def put(self, request, pk=None):
+    """
+    Handle Updating an Object
+    """
+    return Response({'method': 'PUT'})
+
+
+  def patch(self, request, pk=None):
+    """
+    Handle a partial update of an object
+    """
+    return Response({'method': 'PATCH'})
+
+  
+  def delete(self, request, pk=None):
+    """
+    Delete an object
+    """
+    return Response({'method': 'DELETE'})
+        
 class TestApiView(APIView):
   """Another Test Api View"""
   
